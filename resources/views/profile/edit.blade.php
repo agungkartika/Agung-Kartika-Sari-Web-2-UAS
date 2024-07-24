6c610e248076538a04413cb601a5790af36e5be4
agungkartika <agungkartikasari09@gmail.com>
2024-07-24 18:44:32 +0700

    edit profil update

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -2,7 +2,7 @@
     <div class="p-4 mt-14 sm:ml-64">
         <div class="p-4 bg-gray-200 dark:bg-gray-800 rounded-md">
             <div class="flex justify-between">
-                <h1 class="text-2xl text-black self-center dark:text-white">Tambah Laporan</h1>
+                <h1 class="text-2xl text-black self-center dark:text-white">Edit Profil</h1>
             </div>
         </div>
         <div class="p-4 mt-5 bg-gray-200 dark:bg-gray-800 rounded-md">
